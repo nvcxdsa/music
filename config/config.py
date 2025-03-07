3578ed7291dc644b4b4c053547d76c285f6a2767
iamnolimit <manonebillion@gmail.com>
2025-03-08 05:25:14 +0700

    a

--- config/config.py
+++ config/config.py
@@ -75,7 +75,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/gabrielmaialva33/flora-music-bot"
+    "UPSTREAM_REPO", ""
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
